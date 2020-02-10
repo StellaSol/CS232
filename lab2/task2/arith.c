4039afa73c7e241dfe5bfd295f4ca7f5716ac067
ssol1702 <ssolano_1702@hotmail.com>
2020-02-10 12:41:46 -0500

    Completed task2 for lab2

---
+++ lab2/task2/arith.c
@@ -0,0 +1,18 @@
+/*  Example: C program to find area of a circle */
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  float r, a, c;
+
+  printf("Enter radius (in mm):\n");
+  scanf("%f", &r);
+
+  a = PI *(( r * r) / 645.16);
+  c = PI * 2 * (r/25.4);
+
+  printf("Circle's area is %3.2f (sq in).\n", a);
+  printf("Its circumference is %3.2f (in).\n", c);
+}
