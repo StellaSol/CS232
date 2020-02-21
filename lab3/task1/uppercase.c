dde0a58e9b9865b56656b26128a0eef14c8d09ca
ssol1702 <ssolano_1702@hotmail.com>
2020-02-20 22:42:16 -0500

    completed task1 for lab3

--- lab3/task1/uppercase.c
+++ lab3/task1/uppercase.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-/* example:
- *  char str[10090];
-   int ch, n = 0;
+  char str[10090];
+   int ch, n=0,i=0;
 
    while ((ch = getchar()) != EOF && n < 1000) {
       str[n] = ch;
       ++n;
    }
 
-   for (int i = 0; i < n; ++i)
-      putchar(str[i]);
+    for(n=0;str[n];n++)
+      {
+          if(str[n]>=97 && str[n]<=122 ){
+              str[n] = str[n] - 32;
+          }
+      }
+    
+    
+   while(str[i]!='\0'){
+       putchar(str[i]);
+       i++;
+   }
 
-
-   return 0;
-
- */
-
+    
     return 0;
 }
 
