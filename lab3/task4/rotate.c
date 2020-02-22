f0951f77650b8eaf66bd69c4640ea66c26ed1ebf
ssol1702 <ssolano_1702@hotmail.com>
2020-02-21 21:45:35 -0500

    completed task4 for lab3

--- lab3/task4/rotate.c
+++ lab3/task4/rotate.c
@@ -3,17 +3,29 @@
 
 void rotate_4(int *p_a, int*p_b, int*p_c, int* p_d) {
     //TO-DO: please implement left rotate 4 integers by one position
+    int temp=*p_a;
+    *p_a = *p_b;
+    *p_b = *p_c;
+    *p_c = *p_d;
+    *p_d = temp;
 }
 
 int main(int argc, char ** argv) {
     //TO-DO check if there are 4 inputs.
     //print out an error message if there are not enough inputs.
     //hint: use argc
+    if(argc < 5 || argc > 5){
+        printf("Error. Rotate needs four numbers");
+        return 0;
+    }
+    
+    else{
     int a = atoi(argv[1]); //atoi converts string to int
     int b = atoi(argv[2]);
     int c = atoi(argv[3]);
     int d = atoi(argv[4]);
     rotate_4(&a, &b, &c, &d);
     printf("%d %d %d %d\n", a, b, c, d);
+    }
     return 0;
 }
