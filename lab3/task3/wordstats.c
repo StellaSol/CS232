046d5534c5d9260798cd8153d07bb3684d81de3b
ssol1702 <ssolano_1702@hotmail.com>
2020-02-21 19:54:21 -0500

    Completed task3 for lab3

--- lab3/task3/wordstats.c
+++ lab3/task3/wordstats.c
@@ -5,12 +5,39 @@
 int main () {
 
   /* Zero out the array */
-  int letter_frequency[26] = {0};
-  int len, i;
-  char buf[MAX_BUF];
+  int letter_frequency[26] = {0}; //Array for letter frequencies
+  int len=0, i=0,j=0,k;
+  char buf[MAX_BUF];//char array
 
-  do {
-  } while (len > 1);
+  printf("Enter a string");
+  fgets(buf,MAX_BUF,stdin);//taking input for the array
+    
+  len=strlen(buf)-1; //length of the string
+    
+   do {
+      if(len<=MAX_BUF){//TO CHECK IF INPUT IS NOT GREATER THAN THE MAX_BUF
+          if(buf[i]>=97 && buf[i]<=122 ){//This will check frequencies of lowercase
+              k = buf[i] - 97; 
+              letter_frequency[k]++;
+              i++;
+          }
+          else if(buf[i]>=65 && buf[i]<=90 ) { //This will check freq of uppercase
+              k = buf[i] - 65;
+              letter_frequency[k]++;
+              i++;
+          }
+          else{ //For
+              i++;
+          }
+          j++;
+      }
+      
+      else{
+          printf("Only 1024 characters are allowed\n");
+          break;
+      }
+      
+  } while (j<len);
 
   printf("Distribution of letters in corpus:\n");
   for (i = 0; i < 26; i++) {
