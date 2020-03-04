b00bbe4e8488abefbf2e70648c711eb5cd81bc0e
ssol1702 <ssolano_1702@hotmail.com>
2020-03-03 22:35:31 -0500

    mem_bug2 complete

--- lab4/task3/mem_bug2.c
+++ lab4/task3/mem_bug2.c
@@ -3,15 +3,16 @@
 
 int main(int argc, char * argv[]){
 
-  int i, *a;
-
+  int i, *a=NULL;
   a = calloc(10, sizeof(int));
 
-  for(i=0;i <= 10; i++){
+  for(i=0;i < 10; i++){
     a[i] = i;
   }
-  for(i=0;i <= 10; i++){
+
+    for(i=0;i < 10; i++){
     printf("%d\n", a[i]);
   }
+    free(a);
 
 }
