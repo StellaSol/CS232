3ec8d4ee9998c815360d6886a87a6f95f070f9f2
ssol1702 <ssolano_1702@hotmail.com>
2020-03-07 11:45:18 -0500

    completed task1 for lab5

--- lab5/task1/construct_3_structs.c
+++ lab5/task1/construct_3_structs.c
@@ -9,17 +9,36 @@ node_t * setup() {
     //Allocate three more pointers
     //head for the first Node, and temporary pointers node1, node2 and node3
     //Allocate three node pointees and store references to them in the three pointers
+    node_t * node1=(struct snode*)malloc(sizeof(node_t));
+    node_t * node2=(struct snode*)malloc(sizeof(node_t));
+    node_t * node3=(struct snode*)malloc(sizeof(node_t));
+    head = node1;
     //Dereference each pointer to store the appropriate number and string into the length and str fields in its pointee.
-    //Dereference each pointer to access the .next field in its pointee, 
-    //and use pointer assignment to set the .next field to point to the appropriate Node. 
-    
-   return head;
+    strcpy(node1->str, "hello");
+    node1->length=5;
+    strcpy(node2->str, "there");
+    node2->length=5;
+    strcpy(node3->str, "prof");
+    node3->length=4;
+    //Dereference each pointer to access the .next field in its pointee,
+    //and use pointer assignment to set the .next field to point to the appropriate Node.
+    node1->next=node2;
+    node2->next=node3;
+    node3->next=NULL;
+    return head;
 }
 
-void teardown(/*what parameter?*/) {
+void teardown(node_t **head) {
     //TODO: free all dynamic memory you requested.
     //Please complete the prototype of teardown.
     //You are not allowed to use globals
+    node_t *cur = *head;
+    while (cur) {
+        node_t *tmp = cur;
+        cur = cur->next;
+        free(tmp);
+    }
+    *head = NULL;
 }
 
 //You can ignore the following code for testing
@@ -27,7 +46,7 @@ void dump_all(node_t*);
 int main (int argc, char ** argv) {
     node_t * head = setup();
     dump_all(head);
-    teardown(/*what argument?*/);
+    teardown(&head);
     return 0;
 }
 
