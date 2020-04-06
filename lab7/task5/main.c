1d9dcbc595dc14779e279109e3ac51f36d4158fb
ssol1702 <ssolano_1702@hotmail.com>
2020-04-05 21:50:35 -0400

    Finished main.c

--- lab7/task5/main.c
+++ lab7/task5/main.c
@@ -1,13 +1,23 @@
 char password[8] = "secret";
 int main() {
     char input[8];
+    int i = 0;
     printf("please enter your password\n");
     scanf("%s", input);
-
-    if(strcmp(input, password)==0) {
-        return 0;
-    } else {
-        return -1;
+    while(i<=7){
+        if (input[i] < 'A' || input[i]> 'Z' ){
+            i++;
+        }
+        else{
+            input[i]=input[i] + ' ';
+            i++;
+        }
     }
 
+    if(strcmp(input, password)!=0) {
+        return 0;
+    }
+    else{
+        return -1;
+    }
 }
